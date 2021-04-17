513f94e538f8a455a3406a29c418dcaabe1095ad
bensvarnum <bens.varnum@gmail.com>
2021-04-17 12:02:04 -0400

    fix typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
-app.use(express.urlencoded({ extened: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "*",
